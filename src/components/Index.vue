42e8685f1a7c70b80c2cecfd160d3777750e73d9
kanboo <wang.kanboo@gmail.com>
2018-07-01 09:20:10 +0800

    init

---
+++ src/components/Index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="wrapper">
+
+    <header class="header"></header>
+
+    <main class="main">
+      <div class="container">
+        {{ msg }}
+      </div>
+    </main>
+
+    <div is='IndexFooter'></div>
+
+  </div>
+</template>
+
+<script>
+// import { mapState, mapGetters, mapMutations } from 'vuex';
+import FontAwesomeIcon from '@fortawesome/vue-fontawesome';
+import IndexFooter from './IndexFooter';
+
+export default {
+  name: 'Index',
+  components: {
+    FontAwesomeIcon,
+    IndexFooter
+  },
+  data() {
+    return {
+      msg: 'Index'
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lagn="scss">
+</style>
